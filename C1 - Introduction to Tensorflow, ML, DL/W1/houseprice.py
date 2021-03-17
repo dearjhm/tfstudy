e798c5014b17b3d927148742e8a5b85a21924113
jacob.jeong <jacob.jeong@kakaocorp.com>
2021-03-17 17:55:47 +0900

    2주차 작업까지 추가

--- C1 - Introduction to Tensorflow, ML, DL/W1/houseprice.py
+++ C1 - Introduction to Tensorflow, ML, DL/W1/houseprice.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
+ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
+
+model = Sequential(Dense(units=1, input_shape=[1]))
+model.compile(loss="mean_squared_error", optimizer="sgd", metrics=["accuracy"])
+model.fit(xs, ys, epochs=1000)
+
+y_pred = model.predict([7.0, 8.0, 9.0, 10.0, 11.0])
+print(y_pred)
